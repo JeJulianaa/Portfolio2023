34b039f809f2a3847cbacfef80f16bd91ef5383b
JeJulianaa <albtor01@easv365.dk>
2023-04-19 13:08:41 +0200

    1 og 2 container opbyggning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,9 @@
 <template>
   
   <main class="bg-zinc-800" >
+    
     <div class="">
+      
         <div class="b-zinc-500 flex">
           <!--<div class="w-auto md:w-20 sm:w-14 xs:w-11">
 
@@ -15,13 +17,13 @@
           <div class="basis-1/12 xs:basis-2 sm:basis-36">
 
           </div>
-          <div class="basis-1/2 pt-16 2xl:pt-64 xl:pt-48 lg:pt-36 md:pt-28  sm: pt-20  pt-5">
+          <div class="basis-1/2 pt-16 pb-16 2xl:pt-58 xl:pt-48 lg:pt-36 md:pt-28  sm: pt-20  pt-5">
             
             <div class="w-full bg-violet-600">
               <h1 class="homeTextOne text-sky-900 sm:text-violet-800 md:text-orange-500  lg:text-rose-400  2xl:text-green-600 xl:text-red-600"> Mit navn er </h1>
               <h1 class="homeTextOne pt-1 xs:pt-0">Alberte Torbensen</h1>
               <p class="homeTextTwo pt-4 xs:pt-0 pb-4">Jeg er content Creator</p>
-            <button class="homeButtomProjekt text-center p-2 xs:p-4 lg:p-6 lg:mt-10 bg-sky-900 ">Se projekter</button>
+            <button class="w-3/4 homeButtomProjekt text-center  p-4 lg:p-6 lg:mt-10 bg-sky-900 ">Se projekter</button>
           </div>
           </div>
           
@@ -30,40 +32,50 @@
             <img alt="Vue logo"  src="@/assets/billeder/alberte-billede-home.jpg" class="w-full basis-1/2 h-full object-cover" />
           </div>
 
-          <div class="basis-1/12 xs:basis-2 sm:basis-36">
+          <div class="basis-1/4 xs:basis-2 sm:basis-36">
           </div>
         </div>
 
         <hr class="border-2">
-
-        <div class=" right-0 h-80">
+        
+        <div class="ConTaioner h-auto">
+          
           <div class="flex flex-row xs:flex-col sm:flex-row ">
-           <div class="basis-1/12 xs:basis-2 sm:basis-36">
-            1
-          </div>
-
-            <div class="basis-1/2 bg-orange-600">
-              2
+           
+            <div class="basis-1/4 xs:basis-2 sm:basis-36">
+              
+            </div>
+         <div class="parrentTwo flex basis-1/2 bg-orange-600">   
+           
+            <div class="childTwo bg-green-600 py-20">
               <h1 class="text-5xl">
                 Alberte
-              </h1>
-              <p class="text">
-                Som multimediedesigner og content creator bringer 
-                jeg et nyt perspektiv til hvert projekt.
-                 Med færdigheder inden for UX/UI, content creation, 
-                 web development og en dedikation til at holde sig op to-date med de nyeste trends.
+             </h1>
+               <p class="text ">
+               Som multimediedesigner og content creator bringer 
+               jeg et nyt perspektiv til hvert projekt.
+               Med færdigheder inden for UX/UI, content creation, 
+               web development og en dedikation til at holde sig op to-date med de nyeste trends.
               </p>
-              <button class="homeButtomProjekt text-center p-2 xs:p-4 lg:p-6 lg:mt-10 bg-sky-900 ">Om Mig</button>
+              <button class="w-1/2 homeButtomProjekt text-center p-2 xs:p-4 lg:p-6 lg:mt-10 bg-sky-900 ">
+                 Om Mig
+              </button>
+            </div>
+          </div>
+
+          
+          
+
+          <div class="parrentTwo flex basis-1/2 bg-orange-600">   
+            
+           
+           
+          </div>
+          <div class="sideBox basis-1/4 xs:basis-2 sm:basis-36">
+              
             </div>
 
-           
-
-            <div class="basis-1/2 bg-blue-600">
-              3
-          </div>
-          <div class="basis-1/12 xs:basis-2 sm:basis-36">
-            4
-          </div>
+              
         </div>
 
       </div>
