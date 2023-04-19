573de280a7f65af2d652a980dec407a6f9d3df6e
JeJulianaa <albtor01@easv365.dk>
2023-04-19 11:49:26 +0200

    responsive xs til xl
    update flex wrap og flex col sat på ved xs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,36 +6,74 @@
 
 <template>
   
-  <main class="w-full bg-zinc-800" >
-    <div class="b-zinc-500 flex">
-      <div class="w-auto md:w-20 sm:w-14 xs:w-11">
+  <main class="bg-zinc-800" >
+    <div class="">
+        <div class="b-zinc-500 flex">
+          <!--<div class="w-auto md:w-20 sm:w-14 xs:w-11">
+
+          </div>-->
+          <div class="basis-1/12 xs:basis-2 sm:basis-36">
+
+          </div>
+          <div class="basis-1/2 pt-16 2xl:pt-64 xl:pt-48 lg:pt-36 md:pt-28  sm: pt-20  pt-5">
+            
+            <div class="w-full bg-violet-600">
+              <h1 class="homeTextOne text-sky-900 sm:text-violet-800 md:text-orange-500  lg:text-rose-400  2xl:text-green-600 xl:text-red-600"> Mit navn er </h1>
+              <h1 class="homeTextOne pt-1 xs:pt-0">Alberte Torbensen</h1>
+              <p class="homeTextTwo pt-4 xs:pt-0 pb-4">Jeg er content Creator</p>
+            <button class="homeButtomProjekt text-center p-2 xs:p-4 lg:p-6 lg:mt-10 bg-sky-900 ">Se projekter</button>
+          </div>
+          </div>
+          
+          
+          <div class="basis-1/2">
+            <img alt="Vue logo"  src="@/assets/billeder/alberte-billede-home.jpg" class="w-full basis-1/2 h-full object-cover" />
+          </div>
+
+          <div class="basis-1/12 xs:basis-2 sm:basis-36">
+          </div>
+        </div>
+
+        <hr class="border-2">
+
+        <div class=" right-0 h-80">
+          <div class="flex flex-row xs:flex-col sm:flex-row ">
+           <div class="basis-1/12 xs:basis-2 sm:basis-36">
+            1
+          </div>
+
+            <div class="basis-1/2 bg-orange-600">
+              2
+              <h1 class="text-5xl">
+                Alberte
+              </h1>
+              <p class="text">
+                Som multimediedesigner og content creator bringer 
+                jeg et nyt perspektiv til hvert projekt.
+                 Med færdigheder inden for UX/UI, content creation, 
+                 web development og en dedikation til at holde sig op to-date med de nyeste trends.
+              </p>
+              <button class="homeButtomProjekt text-center p-2 xs:p-4 lg:p-6 lg:mt-10 bg-sky-900 ">Om Mig</button>
+            </div>
+
+           
+
+            <div class="basis-1/2 bg-blue-600">
+              3
+          </div>
+          <div class="basis-1/12 xs:basis-2 sm:basis-36">
+            4
+          </div>
+        </div>
 
       </div>
-      <div class="w-2/4  pt-16 2xl:pt-64 xl:pt-48 lg:pt-36 md:pt-28  sm: pt-20  pt-5">
-        
-        <div class="bg-violet-600">
-          <h1 class="homeTextOne text-sky-900 sm:text-violet-800 md:text-orange-500  lg:text-rose-400  2xl:text-green-600 xl:text-red-600"> Mit navn er </h1>
-          <h1 class="homeTextOne pt-1 xs:pt-0">Alberte Torbensen</h1>
-          <p class="homeTextTwo pt-4 xs:pt-0 pb-4">Jeg er content Creator</p>
-         <button class="homeButtomProjekt text-center p-2 xs:p-4 lg:p-6 lg:mt-10 bg-sky-900 ">Se projekter</button>
-      </div>
-      </div>
-      
-      
-      <div class="w-6/12">
-        <img alt="Vue logo" class="w-full object-cover" src="@/assets/billeder/alberte-billede-crop.jpg" />
-      </div>
     </div>
-
-    <div class="rounded border-red-600">
-      
-    </div>
-  </main>
+    </main>
  
 </template>
 
-<style>
-
+<style> 
+    
 
 @media (min-width: 350px) {
   .homeTextOne{
