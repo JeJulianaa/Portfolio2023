c6d78c60d8bfc226d1a7f44ca2dbb31d5106da31
JeJulianaa <albtor01@easv365.dk>
2023-04-22 22:39:59 +0200

    om mig update

--- src/views/OmmigView.vue
+++ src/views/OmmigView.vue
@@ -11,17 +11,45 @@
             <div class="flex flex-wrap justify-center  ">
               <div class="w-full h-auto lg:w-2/5 px-4 mb-4 lg:mb-0  py-4" >
                 <!-- Content for the left column goes here -->
-                <div class="border-2 rounded-lg py-20 h-full">
+                <div class="border-2 rounded-lg py-4 h-full">
                 
-                  <h1> Hvorfor valgte jeg at blive multi medie designer? </h1>
-                  <p class="text pb-8">
-                    inden jeg startet på mit studie har jeg nørdet i adobes programmer siden jeg var 14 år.
-                    Jeg elsker at være computer, tage billeder/video og være kreativ så at være multi medie designer er noget jeg virkelig brænder for!
-                    <br>
-                    
-                  </p>
+                  <h1 class="text-4xl text-center pb-3 text-[#FFE6A7] pt-2">
+                    Beskrive af Mig
+                  </h1>
                  
+                  <h1 class="text-2xl text-[#FFE6A7] pt-2">
+                    dedikeret målorienteret glad Jordnær og kreativ
+                  </h1>
+
+                  <p>Hej! mit navn er Alberte Juliana Torbensen jeg er 23 år og er bosat i Esbjerg.
+                    Jeg brander for at skabe content, om det er marketføringstatigi, webdesign, grafisk design, eller billeder og video så jeg klar på det
+                    hele! 
+                    <br>
+                    jeg har altid haft en indre multimedie designer i mig. 
+                    allerede som 13-14 år fik jeg mit første rigtige kamera og begyndte at nørde i adobe programmerne.
+                    
+                    som person er jeg jordnær og glad også ser jeg altid postiv på tingene.
+                    jeg går ind til alle projekter med en passion på 120%, et projekt bliver aldrig kedligt for mig. 
+                    Jeg synes det er sjovt og spændende at få udfordringer og lære nyt viden.
+                  </p>
+               
+                  <br>
+                  <h1 class="text-2xl text-[#FFE6A7] pt-2">
+                    Samler trådende
+                  </h1>
                 
+                    
+                    <p> 
+                      
+                      i et team arbjede kan jeg både bidage med god humor men samtigdig en
+                       skabe en ro, da jeg er god til at holde struktur/overblik og have mange bolde i luften.
+                       
+                      
+                      jeg er god til at samle trådende sammen, så projekt forløb har en rødtråd. 
+                      når jeg arbejder i et team sørger jeg altid for at komunnikere, og lytte.
+                      Jeg har fra tidligere projekter og jobs både erfaring med team arbejde men også selvstændigt arbejde.
+                      
+                    </p>
               </div>
 
               </div>
@@ -29,9 +57,9 @@
           <div class="w-full lg:w-1/2 px-4">
                 <!-- Content for the right column goes here -->
                 <div class="childTwo   pl-2 py-2 pt-4 h-fit rounded-lg">
-                  <a href="#" class="">
-                    <img src="@/assets/billeder/workex.png" alt="" class="w-full object-container rounded-lg glow-photo-button">
-                  </a>
+                  <div class="w-[50%] childTwo rounded-lg">
+                  <iframe width="949" height="534" src="https://www.youtube.com/embed/2MA9uxZqyaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
                 </div>
                   
               </div>
@@ -49,7 +77,7 @@
         <div class="w-full flex justify-end">
 
 
-          <div class="w-full sections flex flex-wrap w-5/6 justify-end py-2">
+          <div class="w-full sections flex flex-wrap w-5/6  py-2">
             <div class="w-full lg:w-[60%] pb-2 " >
               <!-- Content for the left column goes here -->
               <div class="border-4 border-[#2E2E30] rounded-lg  pt-[2%] px-20 h-full">
@@ -59,37 +87,41 @@
                   mine færdigheder </h1>
                   
                   <p class="text pb-8">
-                    inden jeg startet på mit studie har jeg nørdet i adobes programmer siden jeg var 14 år.
-                    Jeg elsker at være computer, tage billeder/video og være kreativ så at være multi medie designer er noget jeg virkelig brænder for!
+                    Her ser du hvilke ting jeg har færdigheder i, hvis du er interrestert i mine erfaringer eller uddannelser kan du finde dette i min CV som du kan download ved at klikke på knappen "hent CV"
+
                     <br>
+                    <div>
+                      <p>
+                        Content creation
+                          <br>
+                          Braning/rebranding
+                          social 
+                          <br>
+                          medie management
+                          <br>
+                          Grafisk design
+
+                      </p>
+                  </div>
                     
                   </p>
-                
-                  <button class="w-1/2 glow-button homeButtonProjekt  text-center text-[#5F5F5F] border-[#5F5F5F] border-2 rounded-lg p-2 py-4 ml-4">
-                  Hent CV
-                </button>
+                  <div class="flex">
+                      <button class="w-1/2 glow-button homeButtonProjekt  text-center text-[#5F5F5F] border-[#5F5F5F] border-2 rounded-lg p-2 py-4 ml-4">
+                      Hent CV
+                      </button>
+                      <button class="w-1/2 glow-button homeButtonProjekt  text-center text-[#5F5F5F] border-[#5F5F5F] border-2 rounded-lg p-2 py-4 ml-4">
+                        LinkedIn
+                      </button>
+                    </div>
+                 
             </div>
 
             </div>
-            <div class="w-full lg:w-[40%] pt-1">
+            <div class="w-full lg:w-[50%] pt-1">
               <!-- Content for the right column goes here -->
-              <div class="w-full childTwo pl-2 py-2 pt-4  rounded-lg">
-                <h1 class="text-2xl text-[#FFE6A7] pt-2">
-                    dedikeret målorienteret glad Jordnær og kreativ
-                  </h1>
-                <p>
-                  Om det er på en arbejdesplads eller en kunde vil du altid opleve at jeg er en glad og imødekommmende person
-                   som arbejder ambisøst på sine projekter.
-                   Jeg er går ind til alle projekter med en passion på 110%, Jeg aldrig oplevet at få et projekt jeg synes var kedeligt
-                    da jeg synes det er sjovt og spændende at få udfordringer og lære nyt viden.
-                   Hvis du gerne vil vide mere omkring mig så du velkommen til at se mit video CV hvor jeg 
-                   bla. kommer ind over mine færdigheder, uddannelse, jobs og mig som person. 
-                   ellers hvis du 
-                   har spørgsmål er du velkommen til at kontakte mig. 
-                 
-                   
-                  </p>
-              
+              <div class="w-full childTwo pl-2 py-2 pt-4 flex rounded-lg">
+                
+                
               </div>
                 
             </div>
@@ -115,63 +147,14 @@
               </div>
               <div class="w-full lg:w-[40%] pl-2 py-2 pt-1">
                 <!-- Content for the right column goes here -->
-                <div class="w-[46%] childTwo rounded-lg">
-                  <iframe width="742" height="417" src="https://www.youtube.com/embed/2MA9uxZqyaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
+                
                   
               </div>
             </div>
           </div>
 
 
-          <!--section en holdspiller-->
-
-          <div class="w-full flex justify-end ">
-              <div class="boxreverse flex flex-wrap  justify-end pt-1 ">
-                <div class="w-full h-auto lg:w-1/2 px-[8rem] lg:mb-0  py-4 border-4  rounded-lg border-[#2E2E30]" >
-                  <!-- Content for the left column goes here -->
-                  <h1 class="text-2xl text-[#FFE6A7] pt-2">
-                    En holdspiller
-                  </h1>
-                  
-               
-                
-                    <p> 
-                      Vi alle forskellige og vi tænker ikke ens, hvilke jeg kun ser postivtit på. det er sådan man får de bedste 
-                      ideer. Når jeg arbejder i teams, er min rolle ofte koordinatoren, og den der holder struktur/overblik.
-                      jeg elsker at samle alle trådende sammen så det færdig projekt for sin helhed. 
-                      når jeg arbejder i et team sørger jeg for at komunnikere hele tiden, og arbejde som et team så
-                      alle føler sig tilpas i sammenarbejdet. Jeg har fra tidligere jobs både erfaring med team arbejde 
-                      men også selvstændigt
-                      har spørgsmål er du velkommen til at kontakte mig. 
-                    </p>
-                 
-                  
-                  
-                
-                </div>
-                <div class="w-full lg:w-1/2 pl-2">
-                  <!-- Content for the right column goes here -->
-                  <div class="childTwo   pl-1  h-fit rounded-lg">
-                    
-                    <RouterLink to="/portfoliodetail/2">
-                      <a href="#" class="">
-                        <img src="@/assets/billeder/noodles-promotion.jpg" alt="" class="w-full object-container rounded-lg glow-photo-button">
-                      </a>
-                    </RouterLink>
-                  </div>
-                    
-                </div>
-              </div>
-            </div>
-
-
-
-
-            <!--section1 photo-->
-           <div class="h-1 bg-[#1A1A1A]">
-
-          </div>
+         
 
           
   </main>
