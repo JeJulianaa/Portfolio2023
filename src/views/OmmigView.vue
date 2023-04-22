09be7d81a2669a19a06d501d0d45a02d06fb1eea
JeJulianaa <albtor01@easv365.dk>
2023-04-22 20:10:11 +0200

    om mig

--- src/views/OmmigView.vue
+++ src/views/OmmigView.vue
@@ -12,19 +12,23 @@
               <div class="w-full h-auto lg:w-2/5 px-4 mb-4 lg:mb-0  py-4" >
                 <!-- Content for the left column goes here -->
                 <div class="border-2 rounded-lg py-20 h-full">
-                  <h1 class="text-5xl  text-center">
-                    Fremhævede Projekter
-                </h1>
-                <p class="text pb-8">
-                Hvorfor min uddannelse
-                </p>
+                
+                  <h1> Hvorfor valgte jeg at blive multi medie designer? </h1>
+                  <p class="text pb-8">
+                    inden jeg startet på mit studie har jeg nørdet i adobes programmer siden jeg var 14 år.
+                    Jeg elsker at være computer, tage billeder/video og være kreativ så at være multi medie designer er noget jeg virkelig brænder for!
+                    <br>
+                    
+                  </p>
+                 
                 <button class="w-1/2 glow-button homeButtonProjekt  text-center text-[#5F5F5F] border-[#5F5F5F] border-2 rounded-lg p-2 py-4 ml-4">
                   Hent CV
                 </button>
               </div>
 
               </div>
-              <div class="w-full lg:w-1/2 px-4">
+              
+          <div class="w-full lg:w-1/2 px-4">
                 <!-- Content for the right column goes here -->
                 <div class="childTwo   pl-2 py-2 pt-4 h-fit rounded-lg">
                   <a href="#" class="">
@@ -36,23 +40,140 @@
             </div>
           </div>
 
-          <div class="w-full flex justify-center ">
-            <div class="flex flex-wrap justify-center  ">
+          
+
+          <!--section1 photo-->
+        <div class="h-1 bg-[#1A1A1A]">
+
+        </div>
+        <div class="w-full flex justify-end">
+
+
+          <div class="w-full sections flex flex-wrap w-5/6 justify-end ">
+            <div class="w-full lg:w-[60%] pb-2  lg: py-5" >
+              <!-- Content for the left column goes here -->
+              <div class="border-4 border-[#2E2E30] rounded-lg text-center pt-[11%] h-full">
+                <h1 class="text-7xl  text-center text-[#E44A8C]">
+                  <h1> Hvorfor valgte jeg at blive multi medie designer? </h1>
+                  <p class="text pb-8">
+                    inden jeg startet på mit studie har jeg nørdet i adobes programmer siden jeg var 14 år.
+                    Jeg elsker at være computer, tage billeder/video og være kreativ så at være multi medie designer er noget jeg virkelig brænder for!
+                    <br>
+                    
+                  </p>
+                </h1>
+                <h1 class="text-5xl text-[#FFE6A7] pt-2">
+                    dedikeret målorienteret glad Jordnær og kreativ
+                  </h1>
+                <p>
+                  Om det er på en arbejdesplads eller en kunde vil du altid opleve at jeg er en glad og imødekommmende person
+                   som arbejder ambisøst på sine projekter.
+                   Jeg er går ind til alle projekter med en passion på 110%, Jeg aldrig oplevet at få et projekt jeg synes var kedeligt
+                    da jeg synes det er sjovt og spændende at få udfordringer og lære nyt viden.
+                   Hvis du gerne vil vide mere omkring mig så du velkommen til at se mit video CV hvor jeg 
+                   bla. kommer ind over mine færdigheder, uddannelse, jobs og mig som person. 
+                   ellers hvis du 
+                   har spørgsmål er du velkommen til at kontakte mig. 
+                 
+                   
+                  </p>
+              <p class="text pb-8">
+
+              </p>
               
-              
-              </div>
-              <div class="w-full lg:w-1/2 px-4">
-                <!-- Content for the right column goes here -->
-                <div class="childTwo   pl-2 py-2 pt-4 h-fit rounded-lg">
+            </div>
+            </div>
+            <div class="w-full lg:w-[40%] pt-1">
+              <!-- Content for the right column goes here -->
+              <div class="w-full childTwo pl-2 py-2 pt-4  rounded-lg">
+                <RouterLink to="/portfoliodetail/1">
                   <a href="#" class="">
-                    <img src="@/assets/billeder/workex.png" alt="" class="w-full object-container rounded-lg glow-photo-button">
+                    <img src="@/assets/billeder/Mockup-pc-dreams.jpg" alt="" class="w-full object-container rounded-lg glow-photo-button">
                   </a>
+                </RouterLink>
+              </div>
+                
+            </div>
+          </div>
+        </div>
+          
+
+
+          <!--section 2 big photo-->
+
+          <div class="w-full flex justify-end ">
+              <div class="boxreverse flex flex-wrap  justify-end pt-1 ">
+                <div class="w-full h-auto lg:w-1/2 px-[8rem] lg:mb-0  py-4 border-4  rounded-lg border-[#2E2E30]" >
+                  <!-- Content for the left column goes here -->
+                  <h1 class="text-5xl text-[#FFE6A7] pt-2">
+                    dedikeret målorienteret glad Jordnær og kreativ
+                  </h1>
+                  
+                 <p>
+                  
+                 
+                   <h1>En holdspiller</h1>
+                    <p> 
+                      Vi alle forskellige og vi tænker ikke ens, hvilke jeg kun ser postivtit på. det er sådan man får de bedste 
+                      ideer. Når jeg arbejder i teams, er min rolle ofte koordinatoren, og den der holder struktur/overblik.
+                      jeg elsker at samle alle trådende sammen så det færdig projekt for sin helhed. 
+                      når jeg arbejder i et team sørger jeg for at komunnikere hele tiden, og arbejde som et team så
+                      alle føler sig tilpas i sammenarbejdet. Jeg har fra tidligere jobs både erfaring med team arbejde 
+                      men også selvstændigt
+                      har spørgsmål er du velkommen til at kontakte mig. 
+                    </p>
+                  </p>
+                  
+                  
+                
+                </div>
+                <div class="w-full lg:w-1/2 pl-2">
+                  <!-- Content for the right column goes here -->
+                  <div class="childTwo   pl-1  h-fit rounded-lg">
+                    
+                    <RouterLink to="/portfoliodetail/2">
+                      <a href="#" class="">
+                        <img src="@/assets/billeder/noodles-promotion.jpg" alt="" class="w-full object-container rounded-lg glow-photo-button">
+                      </a>
+                    </RouterLink>
+                  </div>
+                    
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <!--section1 photo-->
+           <div class="h-1 bg-[#1A1A1A]">
+
+          </div>
+          <div class="w-full flex justify-end">
+
+
+            <div class="w-full sections flex flex-wrap w-5/6 justify-end ">
+              <div class="w-full lg:w-[55%] pb-2  lg: py-5" >
+                <!-- Content for the left column goes here -->
+                <div class="border-4 border-[#2E2E30] rounded-lg text-center pt-[11%] h-full">
+                  <h1 class="text-7xl  text-center text-[#E44A8C]">
+                  Video <h1 class="text-7xl px-20 mb-5 lg:mb-0 pb-8 py-5 text-[#0091C2]">CV</h1>
+                  </h1>
+                <p class="text pb-8">
+
+                </p>
+                
+              </div>
+              </div>
+              <div class="w-full lg:w-[44%] pt-1">
+                <!-- Content for the right column goes here -->
+                <div class="w-full childTwo pl-2 py-2 pt-4  rounded-lg">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/2MA9uxZqyaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
                   
               </div>
             </div>
-          
-        
+          </div>
   </main>
 </template>
 
